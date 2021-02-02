444d1db80522bd9d17c4253d5c36d49ce90506a6
Ken Tauchi <kentauchica@gmail.com>
2021-02-01 23:10:33 -0800

    add nore text to the home page

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Capstone ProjectCapstone Project
+          Capstone ProjectCapstone Project!
         </a>
       </header>
     </div>
