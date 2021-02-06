e09b39956b721ac2aa9866d22c5df888f4f04c09
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-02-06 06:25:28 -0800

    CAP-104 Implement redux

--- front-end/src/components/Auth/SigninOrSignup/Signup/Signup.js
+++ front-end/src/components/Auth/SigninOrSignup/Signup/Signup.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
+import { useDispatch } from 'react-redux';
+import { userSigninSignup } from '../../../../redux/user/user.actions';
 
 const Signup = (props) => {
+    const [userType, setUserType] = useState("company");
+    const dispatch = useDispatch();
+
+    const signupTest = () => {
+        dispatch(userSigninSignup(userType, 123456, "ThisIsDummyToken"));
+        props.history.replace(`/auth`);
+    }
+
     return (
         <div>
-            <button>Signup</button>
+            <button onClick={signupTest}>Signup</button>
             <p onClick={props.goToSignIN}>Go to Signin</p>
         </div>
     );
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -4,13 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { store } from './redux/store'
 
 import './sass/style.scss';
 
 ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
   document.getElementById('root')
 );
 
