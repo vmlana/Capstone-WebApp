6b16cb2226b67ab1f8cbbc52625815a7e90f9dc8
vmlana <vmlana@gmail.com>
2021-02-10 22:30:57 -0800

    CAP-114 Created api/v1/playlists

--- routes/v1.js
+++ routes/v1.js
@@ -3,10 +3,12 @@ const router  = express.Router();
 
 const {getCategories} = require("../controllers/categoryController.js");
 const {getCompany} = require("../controllers/companyController.js");
+const {getPlaylists} = require("../controllers/playlistController.js");
 
 // Routes to capstone api - Version 1
 router
     .get("/categories", getCategories)
-    .get("/company", getCompany);    
+    .get("/company", getCompany)  
+    .get("/playlists", getPlaylists);     
 
 module.exports = router;
