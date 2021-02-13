5b459fd04c33359e177ed2cbdb9e67221df18e48
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-02-12 16:18:25 -0800

    CAP-128 Add folder to s3 API

--- controllers/s3StorageController.js
+++ controllers/s3StorageController.js
@@ -16,13 +16,15 @@ exports.postS3Storage = (req, res) => {
     const s3 = new aws.S3();  // Create a new instance of S3
     const fileName = req.body.fileName;
     let fileType = req.body.fileType;
+    let folderName = "images";
     if (fileType === "mp4" || fileType === "mov") {
         fileType = "video/" + fileType;
+        folderName = "videos"
     }
     // Set up the payload of what we are sending to the S3 api
     const s3Params = {
         Bucket: S3_BUCKET,
-        Key: fileName,
+        Key: folderName + '/' + fileName,
         Expires: 500,
         ContentType: fileType,
         ACL: 'public-read'
@@ -37,7 +39,7 @@ exports.postS3Storage = (req, res) => {
         // Data payload of what we are sending back, the url of the signedRequest and a URL where we can access the content after its saved. 
         const returnData = {
         signedRequest: data,
-        url: `https://${S3_BUCKET}.s3.amazonaws.com/${fileName}`
+        url: `https://${S3_BUCKET}.s3.amazonaws.com/${folderName}/${fileName}`
         };
 
         // Send it all back
