62596834e5d2f9ae8adbe572378908ebed80ae8c
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-03-13 00:50:06 -0800

    CAP-206 Change API URL to https in front-end code

--- front-end/src/services/apiUrl.js
+++ front-end/src/services/apiUrl.js
@@ -1,7 +1,7 @@
 // console.log(process.env.NODE_ENV);
 
-export const apiUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000/api/v1" : "/api/v1";
+export const apiUrl = process.env.NODE_ENV === "development" ? "http://localhost:3000/api/v1" : "https://pivotcare.ca/api/v1";
 
 // export const BASE_URL = "http://localhost:3001"
 
-export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "/";
+export const BASE_URL = process.env.NODE_ENV === "development" ? "http://localhost:3001" : "https://pivotcare.ca/";
