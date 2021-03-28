bfd1056911a100306baf618fe0d3b2be686a068d
TOMO-YOSHI <lucky.paddy10@gmail.com>
2021-03-27 19:11:54 -0700

    CAP-263 Apply token validation to API and front-end

--- front-end/src/components/Auth/CompanyConfig/Account/Account.js
+++ front-end/src/components/Auth/CompanyConfig/Account/Account.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { Link } from "react-router-dom";
 import styled from "styled-components";
 import * as FAIcons from "react-icons/fa";
 
